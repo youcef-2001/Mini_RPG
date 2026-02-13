dfa11a655230e9207dbe9375690e7b0769bf97e3
youcef-2001 <ybaleh13@gmail.com>
2026-02-13 10:48:10 +0100

    Refactor Environnement class and implement EnvironnementBuilder methods; add GameContext class

--- src/Model/Environnement.py
+++ src/Model/Environnement.py
@@ -1,16 +1,36 @@
+import random
 from Model.Event import Walking
 
 
 class Environnement:
 
-   def __init__(self):
-      self.event= Walking()
+    def __init__(self):
+        self.event= Walking()
+        self.eventlist= []
+        self.eventcounter=0
+        self.title=""
 
+
+    def lancer(self):
+       self.eventcounter+=1
 
 
 class EnvironnementBuilder:
 
-   def __init__(self) :
-      pass
-   
-    
+    def __init__(self) :
+        self.env : Environnement
+    def reset(self):
+        self.env= Environnement()
+    def SetTitle(self,title):
+        self.env.title=title
+    def setBoss(self,env: Environnement):
+        env.eventlist.append("boss")
+    def AddEventlist(self,number):
+        for i in range(0,number):
+            #randomiser apres les event
+            self.env.eventlist.append("combat")# combat , marchand,quest, coffre
+    def insertKey(self):
+        self.env.eventlist.insert(random.randint(0,self.env.eventlist.__len__()),"key")
+
+
+      
